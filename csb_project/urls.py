a4e519123dc603065223305504eff8db2ee64947
Jusa Mankki <jusamankki@gmail.com>
2022-03-08 14:59:41 +0200

    fix

--- csb_project/urls.py
+++ csb_project/urls.py
@@ -13,15 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signin/', views.sign_in, name='signin'),
-    path('signup/', views.sign_up, name='signup'),
+    path('signin', views.sign_in, name='signin'),
+    path('signup', views.sign_up, name='signup'),
     path('user/', views.user_view, name='user_view'),
     path('send_message/', views.send_message, name='send_message'),
     path('update_password/', views.update_password, name='update_password'),
